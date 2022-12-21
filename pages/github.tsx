3e86b9690bd8202d8c2821297c94f65716428392
Atsuhiro Endo <atsuhiro.e@affinidi.com>
2022-12-21 23:27:03 +0100

    feat: style with tailwindcss

--- pages/github.tsx
+++ pages/github.tsx
@@ -1,12 +1,12 @@
 import { GetServerSideProps } from "next";
 import { FC, useState } from "react";
+import Link from "next/link";
+import { RiBracesFill, RiUser5Line } from "react-icons/ri";
 
 import { GithubTokenResponse } from "./api/github/token";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { GithubUser } from "../types/github";
-
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
+import GitIcon from "../components/icons/GitIcon";
 
 type GithubProps = GithubTokenResponse;
 
@@ -41,34 +41,97 @@ const Github: FC<GithubProps> = (props) => {
   };
 
   return (
-    <main className={styles.main}>
-      <h1>Fetch your data from Github</h1>
-      <div>
-        <h2>Access Token</h2>
-        <pre>{storedToken || token}</pre>
+    <main className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="text-center mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
+            Fetch your data from Github
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+            Access Token
+          </p>
+          <pre>{storedToken || token}</pre>
+          <div className="flex mt-6 justify-center">
+            <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+          </div>
+        </div>
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+          <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <RiUser5Line className="h-10 w-10" />
+            </div>
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium">
+                User
+              </h2>
+              <button
+                className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                onClick={handleGetUserData}
+              >
+                Get Data
+              </button>
+              {user && (
+                <p className="leading-relaxed text-base">
+                  <pre>{JSON.stringify(user, undefined, 2)}</pre>
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <GitIcon className="h-10 w-10" />
+            </div>
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium">
+                Repos
+              </h2>
+              <button
+                className="flex mx-auto mt-8 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                onClick={handleGetUserRepos}
+              >
+                Get Repos
+              </button>
+              {repos && (
+                <p className="leading-relaxed text-base">
+                  <pre>{JSON.stringify(repos, undefined, 2)}</pre>
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+              <RiBracesFill className="h-10 w-10" />
+            </div>
+            <div className="flex-grow">
+              <h2 className="text-gray-900 text-lg title-font font-medium">
+                Programming languages
+              </h2>
+              <button
+                className="flex mx-auto mt-8 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                onClick={handleGetUserReposLanguages}
+              >
+                Get programming languages
+              </button>
+              {languages && (
+                <p className="leading-relaxed text-base">
+                  <pre>{JSON.stringify(languages, undefined, 2)}</pre>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-16 justify-center">
+          <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+        </div>
+        <div className="flex flex-col justify-center items-center mt-16">
+          <div>Or</div>
+          <Link href="/issue-vc">
+            <button className="flex mx-auto mt-8 text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-800 rounded text-lg">
+              Issue your developer VC
+            </button>
+          </Link>
+        </div>
       </div>
-      <button onClick={handleGetUserData}>Get User Data</button>
-      {user && (
-        <p>
-          <pre>{JSON.stringify(user, undefined, 2)}</pre>
-        </p>
-      )}
-      <button onClick={handleGetUserRepos}>Get User Repos</button>
-      {repos && (
-        <p>
-          <pre>{JSON.stringify(repos, undefined, 2)}</pre>
-        </p>
-      )}
-      <button onClick={handleGetUserReposLanguages}>
-        Get User&apos;s programming languages
-      </button>
-      {languages && (
-        <p>
-          <pre>{JSON.stringify(languages, undefined, 2)}</pre>
-        </p>
-      )}
-      <h1>Or issue your data developer VC</h1>
-      <Link href="/issue-vc">To VS Issuer form</Link>
     </main>
   );
 };
